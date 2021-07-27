c904eb53706c62c8d5c64c98cf829d7d3d65d8ce
ArtemyAndrienko <artemy.Andrienko@gmail.com>
2021-07-27 18:54:50 +0000

    init commit

---
+++ utils.go
@@ -0,0 +1,19 @@
+package utils
+
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
